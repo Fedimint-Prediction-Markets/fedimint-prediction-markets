d3be80256325d29e38b30f978d4c4c18e746fe94
toyota-corolla0 <toyota_corolla0@proton.me>
2024-01-15 01:07:13 -0600

    fix: client module was being dropped because of api versioning.

--- fedimint-prediction-markets-server/src/lib.rs
+++ fedimint-prediction-markets-server/src/lib.rs
@@ -217,7 +217,7 @@ impl ServerModuleInit for PredictionMarketsInit {
     }
 
     fn supported_api_versions(&self) -> SupportedModuleApiVersions {
-        SupportedModuleApiVersions::from_raw((u32::MAX, 0), (0, 0), &[(0, 0)])
+        SupportedModuleApiVersions::from_raw((2, 0), (2, 0), &[(0, 0)])
     }
 
     /// Initialize the module
