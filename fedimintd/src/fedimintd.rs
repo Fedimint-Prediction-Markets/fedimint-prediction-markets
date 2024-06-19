7800677bc08b1622fc0e54bb79437f280662eb85
toyota-corolla0 <toyota_corolla0@proton.me>
2024-06-18 19:37:34 -0500

    chore: merge upstream

--- fedimintd/src/fedimintd.rs
+++ fedimintd/src/fedimintd.rs
@@ -1,12 +1,17 @@
-use fedimintd::fedimintd::Fedimintd;
+use fedimint_core::core::ModuleKind;
+use fedimint_core::fedimint_build_code_version_env;
+use fedimint_dummy_common::config::DummyGenParams;
+use fedimint_dummy_server::DummyInit;
+use fedimintd::Fedimintd;
+
+const KIND: ModuleKind = ModuleKind::from_static_str("dummy");
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    Fedimintd::new()?
+    Fedimintd::new(fedimint_build_code_version_env!())?
         .with_default_modules()
-        .with_module(fedimint_prediction_markets_server::PredictionMarketsInit)
-        .with_extra_module_inits_params(
-            3,
+        .with_module_kind(fedimint_prediction_markets_server::PredictionMarketsInit)
+        .with_module_instance(
             fedimint_prediction_markets_common::KIND,
             fedimint_prediction_markets_common::config::PredictionMarketsGenParams::default(),
         )
