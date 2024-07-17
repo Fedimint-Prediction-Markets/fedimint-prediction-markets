4807997d3140d5b099e4d8f6a3bc00b30f39676f
toyota-corolla0 <toyota_corolla0@proton.me>
2024-07-17 00:13:48 -0500

    fix: remove tests from merge + remove unused imports in fedimintd

--- fedimintd/src/fedimintd.rs
+++ fedimintd/src/fedimintd.rs
@@ -1,10 +1,5 @@
-use fedimint_core::core::ModuleKind;
 use fedimint_core::fedimint_build_code_version_env;
-use fedimint_dummy_common::config::DummyGenParams;
-use fedimint_dummy_server::DummyInit;
 use fedimintd::Fedimintd;
-
-const KIND: ModuleKind = ModuleKind::from_static_str("dummy");
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
